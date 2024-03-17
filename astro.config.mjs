50e3def37156840559ba5704d406656cd13cbfe3
joel <tchindajoel25120@gmail.com>
2024-03-17 20:16:49 +0100

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://TCHINDAJOEL/portfolioJoel.github.io',
+  site: 'https://TCHINDAJOEL/portfolioJoel.github.io',
+  output: "server",
+  adapter: netlify()
 });
